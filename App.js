2647072a1ea7971f610c33acbe334e9c9315ede9
vjtrollkilla <vjsingh208000@gmail.com>
2023-08-12 19:55:56 -0700

    Setup files for car detection app

--- App.js
+++ App.js
@@ -1,20 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Text, View, Image, StyleSheet } from "react-native";
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [ADCValue, setADCVal] = useState();
+  const setLargeBox = () => {
+    return {
+      marginTop: 10,
+      borderWidth: 2,
+      width: 100,
+      height: 20,
+    };
+  };
+  const getMovies = async () => {
+    try {
+      const response = await fetch("http://192.168.1.110/");
+      const json = await response.json();
+      setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ backgroundColor: "white", flex: 1, paddingTop: 150, paddingLeft: 20 }}>
+      <View style={styles.group}>
+        <View style={{ padding: 10 }}>
+          <View style={styles.boxLarge}></View>
+          <View style={styles.boxMedium}></View>
+          <View style={styles.boxSmall}></View>
+        </View>
+        <View style={{ padding: 10 }}>
+          <View style={styles.boxLarge}></View>
+          <View style={styles.boxMedium}></View>
+          <View style={styles.boxSmall}></View>
+        </View>
+        <View style={{ padding: 10 }}>
+          <View style={styles.boxLarge}></View>
+          <View style={styles.boxMedium}></View>
+          <View style={styles.boxSmall}></View>
+        </View>
+      </View>
+
+      <Image style={styles.logo} source={require("./assets/carImage.png")} />
+      <View style={styles.group}>
+        <View style={{ padding: 10 }}>
+          <View style={styles.boxSmall}></View>
+          <View style={styles.boxMedium}></View>
+          <View style={styles.boxLarge}></View>
+        </View>
+        <View style={{ padding: 10 }}>
+          <View style={styles.boxSmall}></View>
+          <View style={styles.boxMedium}></View>
+          <View style={styles.boxLarge}></View>
+        </View>
+        <View style={{ padding: 10 }}>
+          <View style={styles.boxSmall}></View>
+          <View style={styles.boxMedium}></View>
+          <View style={styles.boxLarge}></View>
+        </View>
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
+  boxLarge: {
+    marginTop: 10,
+    borderWidth: 2,
+    width: 100,
+    height: 20,
+  },
+  boxMedium: {
+    marginTop: 10,
+    borderWidth: 2,
+    width: 80,
+    height: 20,
+  },
+  boxSmall: {
+    marginTop: 10,
+    borderWidth: 2,
+    width: 50,
+    height: 20,
+  },
+  group: {
+    flexDirection: "row",
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    borderWidth: 1,
+    marginTop: 20,
+    marginLeft: 100,
+    width: 100,
+    height: 200,
   },
 });
